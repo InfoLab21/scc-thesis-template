1dfa95e2ea25c1819820c83706225f14ef3c754a
Andrew Moore <andrew.p.moore94@gmail.com>
2021-05-14 15:12:05 +0100

    no longer need some of the clean up

--- compile_latex.sh
+++ compile_latex.sh
@@ -6,6 +6,4 @@ pdflatex -shell-escape -output-directory=./output_directory main.tex
 pdflatex -shell-escape -output-directory=./output_directory main.tex
 cp ./output_directory/main.pdf .
 rm -r ./output_directory/main*
-rm main.bbl
-rm main.blg
 rm main-words.sum
